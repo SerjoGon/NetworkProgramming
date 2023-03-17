7a00de6d5a09f2ebeb4a38410ce053bd76cb9bba
mr.serhioru@mail.ru <mr.serhioru@mail.ru>
2023-03-17 20:00:41 +0300

    multicast

---
+++ NetWorkProgramming/WF_Multicast/ClientMulticast.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_Multicast
+{
+    public partial class ClientMulticast : Form
+    {
+        public ClientMulticast()
+        {
+            InitializeComponent();
+            Thread reciverTrhread = new Thread(new ThreadStart(Reciver));
+            reciverTrhread.IsBackground = true;
+            reciverTrhread.Start();
+        }
+        delegate void receiverDelegate(string text);
+        void RecieveDel(string text)
+        {
+            richTextBox1.Text= text;
+        }
+         void Reciver()
+        {
+            while(true)
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPEndPoint point = new IPEndPoint(IPAddress.Any, 4567);
+                socket.Bind(point);
+                IPAddress address = IPAddress.Parse("234.5.5.5");
+                socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(address,IPAddress.Any));
+                byte[] buffer = new byte[1024];
+                socket.Receive(buffer);
+                richTextBox1.Invoke(new receiverDelegate(RecieveDel),Encoding.UTF8.GetString(buffer));
+                socket.Close();
+            }
+        }
+    }
+}
---
+++ NetWorkProgramming/WF_multicast_Server/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_multicast_Server
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+        static string message = "";
+        Thread sender = new Thread(new ThreadStart(Sender));
+        static void Sender()
+        {
+            while (true)
+            {
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+                IPAddress dest = IPAddress.Parse("234.5.5.5");
+                socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(dest));
+                IPEndPoint point = new IPEndPoint(dest, 4567);
+                socket.Connect(point);
+                socket.Send(Encoding.UTF8.GetBytes(message));
+                socket.Close();
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            message = richTextBox1.Text;
+        }
+    }
+}
